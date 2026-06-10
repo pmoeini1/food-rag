9a9a89d1f18cdca70a0a03a64a281f76896d050b
pmoeini1 <pmoeini@uwo.ca>
2026-06-10 12:02:23 -0400

    recursive splitting

--- loadDocuments.py
+++ loadDocuments.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_experimental.text_splitter import SemanticChunker
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from pathlib import Path
@@ -10,16 +10,25 @@ for pdf in Path("PDFs").glob("*.pdf"):
     loader = PyPDFLoader(str(pdf))
     documents.extend(loader.load())
 
-embeddingModel = HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+print("PDFs loaded")
 
-text_splitter = SemanticChunker(embeddingModel, breakpoint_threshold_type="percentile")
+embeddingModel = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 
 chunks = text_splitter.split_documents(documents)
 
-vector_store = Chroma.from_documents(
-    documents=chunks,
-    embedding=embeddingModel,
+print("text split")
+
+vector_store = Chroma(
+    embedding_function=embeddingModel,
     persist_directory="./nutrition_chroma"
 )
+
+print("create vector db")
+
+vector_store.add_documents(chunks)
+
+print("added to vector db")
 
 vector_store.persist()
