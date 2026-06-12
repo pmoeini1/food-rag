f41cf767a685191ce96af622d089b6916681f8d6
pmoeini1 <pmoeini@uwo.ca>
2026-06-12 10:51:35 -0400

    increase speed

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from langchain_chroma import Chroma
 from transformers import pipeline
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import torch
 
 app = FastAPI()
 
@@ -18,14 +19,16 @@ vector_store = Chroma(persist_directory="./nutrition_chroma", embedding_function
 
 retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 8, "fetch_k": 20})
 
-pipe = pipeline("text-generation", model="Qwen/Qwen2.5-7B-Instruct")
+pipe = pipeline("text-generation", model="Qwen/Qwen2.5-1.5B-Instruct", device_map="auto", dtype=torch.float32)
 
 @app.post("/ask")
 def ask(req: QueryRequest):
 
     docs = retriever.invoke(req.question)
 
-    context = "\n\n".join(doc.page_content for doc in docs)
+
+    context = "\n\n".join(doc.page_content[:300] for doc in docs[:3])
+
 
     prompt = f"""
         You are a nutrition research assistant. If you can't answer the question given the context, say "I don't know".
@@ -38,8 +41,8 @@ def ask(req: QueryRequest):
 
         Answer: 
     """
-    output = pipe(prompt, max_new_tokens=30, do_sample=False)
-    answer = output[0]["generated_text"]
+    output = pipe(prompt, max_new_tokens=30, do_sample=False, return_full_text=False)
+    answer = output[0]["generated_text"].split(".")[0]
 
     return {
         "answer": answer,
